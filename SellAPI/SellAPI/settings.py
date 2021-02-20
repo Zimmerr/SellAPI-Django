6a565ed6653f854ade593e7333d168cf81139971
Luis <lhzimmermann@gmailc.om>
2021-02-20 11:07:03 -0300

    Configurando váriaveis de ambiente pra usar o Postgres

--- SellAPI/SellAPI/settings.py
+++ SellAPI/SellAPI/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -82,6 +86,16 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#  'default': {
+#      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#      'NAME': env('DATABASE_NAME'),
+#      'USER': env('DATABASE_USER'),
+#      'PASSWORD': env('DATABASE_PASSWORD'),
+#      'HOST': env('DATABASE_HOST'),
+#      'PORT': env('DATABASE_PORT'),
+#  }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
